e3c7f8c68fb37f290f7b1c5ce0e3a6969e0b8efe
stevepapas <steve.papa@outlook.com>
2026-05-21 01:28:19 +1000

    feat: add simplified Pages collection compatible with existing MongoDB data

---
+++ src/collections/Pages.ts
@@ -0,0 +1,33 @@
+import type { CollectionConfig } from 'payload'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    useAsTitle: 'title',
+    defaultColumns: ['title', 'slug', 'updatedAt'],
+  },
+  versions: {
+    drafts: true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      admin: {
+        position: 'sidebar',
+      },
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+      admin: {
+        position: 'sidebar',
+      },
+    },
+  ],
+}
