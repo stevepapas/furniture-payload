b1fbad44a27836df140fb71ee45cff993c1b087e
stevepapas <steve.papa@outlook.com>
2026-05-20 19:13:05 +1000

    init: scaffold Payload CMS + Next.js portfolio project

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+}
+
+export default withPayload(nextConfig)
