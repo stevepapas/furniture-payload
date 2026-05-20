fcce1aa1528ff7b39c5a249527af3aa06f50233b
Steve Papa <kremedia@gmail.com>
2026-05-20 23:44:40 +1000

    fix: set outputFileTracingRoot to silence workspace root confusion

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.resolve(dirname),
   images: {
     remotePatterns: [
       {
